d63996f774d4a1049c445a883a0b80aedccc5be0
Luke Vander Poel <airscout99@gmail.com>
2016-08-29 07:24:10 -0700

    Dom and json work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 $( document ).ready(function(){
-  var mymap = L.map('mapid').setView([47.6149943,-122.475988,11], 13);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVrZXZwb2VsIiwiYSI6ImNpcXgyYTJibTAxZTNmc2txMHNkM216ZnkifQ.qig439qLZweqLXTkTjOmFQ', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 10,
-    id: 'lukevpoel',
-    accessToken: 'pk.eyJ1IjoibHVrZXZwb2VsIiwiYSI6ImNpcXgyYTJibTAxZTNmc2txMHNkM216ZnkifQ.qig439qLZweqLXTkTjOmFQ'
-  }).addTo(mymap);
+  //start to pull JSON to fill the events parts
+  $.getJSON("js/events.json", function ( data ) {
+    document.getElementById('pc-first-cal-title').innerHTML = data[0].title;
+    document.getElementById('mob-first-cal-title').innerHTML = data[0].title;
+    document.getElementById('second-cal-title').innerHTML = data[1].title;
+    document.getElementById('third-cal-title').innerHTML = data[2].title;
 
+    //check to see if data is being pulled
+    console.log( data[0].title );
+
+
+      var mymap = L.map('mapid').setView([47.6149943,-122.475988,11], 10);
+
+      L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVrZXZwb2VsIiwiYSI6ImNpcXgyYTJibTAxZTNmc2txMHNkM216ZnkifQ.qig439qLZweqLXTkTjOmFQ', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        minZoom: 8,
+        id: 'lukevpoel',
+        accessToken: 'pk.eyJ1IjoibHVrZXZwb2VsIiwiYSI6ImNpcXgyYTJibTAxZTNmc2txMHNkM216ZnkifQ.qig439qLZweqLXTkTjOmFQ'
+      }).addTo(mymap);
+      var firstMarker = L.marker([data[0].latitude, data[0].longitude]).bindPopup(data[0].title).addTo(mymap);
+      var secondMarker = L.marker([data[1].latitude, data[1].longitude]).bindPopup(data[1].title).addTo(mymap);
+      var thirdMarker = L.marker([data[2].latitude, data[2].longitude]).bindPopup(data[2].title).addTo(mymap);
+
+
+
+  });
 });
